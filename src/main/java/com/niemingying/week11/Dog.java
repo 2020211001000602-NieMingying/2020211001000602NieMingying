ab32854781458ff552afe403e1e5597e8e609add
NieMingYing <2521667170@qq.com>
2022-05-22 14:57:29 +0800

    week 11 week 12

---
+++ src/main/java/com/niemingying/week11/Dog.java
@@ -0,0 +1,13 @@
+package com.niemingying.week11;
+
+public class Dog {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
